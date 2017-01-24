722353f25f74c3fa389e7f7d89d9605962a3f434
agustin <agustin.ideasyproyectos@gmail.com>
2017-01-24 17:14:44 -0600

    First Commit
    Documentacion de tabla y ejemplo de uso

--- README.md
+++ README.md
@@ -1,2 +1,155 @@
-# Vue-Dynamic-Table
-Vue component for use tables
+# Vue-Dynamic-Table 
+
+Componente para manejar tablas.
+
+##Instalación
+
+Debes de importar el archivo del componente despues de importar vuejs
+
+```
+<script src="vue-dynamic-table.js"></script>
+
+```
+
+Tenemos que agregar el codigo del componente dentro de nuestro componente vue
+
+```
+<vue-dynamic-table
+    v-bind:columns="myColumns"
+    v-bind:data-model="myDataModel">
+</vue-dynamic-table>
+
+```
+
+Para que el componente funcione tiene que tener como minimo la opcion de columns y la opcion de data-model.
+Las cuales son arreglos de objetos.
+
+
+```
+var TestComponent = new Vue({
+	el: '#test-component',
+	data: {
+    	myColumns: [
+        	{ name:'col1', title:'Column 1', visible: true },
+        	{ name:'col2', title:'Column 2', visible: true },
+        	{ name:'col3', title:'Column 3', visible: true },
+        	{ name:'col4', title:'Column 4', visible: true }
+        ],
+        myDataModel: [
+        	{ col1: 'Value 1, 1', col2: 'Value 1, 2', col3: 'Value 1, 3', col4: 'Value 1, 4' },
+        	{ col1: 'Value 2, 1', col2: 'Value 2, 2', col3: 'Value 2, 3', col4: 'Value 2, 4' },
+        	{ col1: 'Value 3, 1', col2: 'Value 3, 2', col3: 'Value 3, 3', col4: 'Value 3, 4' },
+        	{ col1: 'Value 4, 1', col2: 'Value 4, 2', col3: 'Value 4, 3', col4: 'Value 4, 4' }
+        ]
+    }
+});
+```
+
+Cada objeto del array que se le pasa a a la tabla por la propiedad columns, representa una columna de la tabla.
+Cada objeto del array que se le pasa a a la tabla por la propiedad data-model, representa una fila de la tabla.
+
+####Objeto Columna
+
+El objeto columna como minimo necesita dos propiedades para funcionar. Que son el name, title y visible.
+```
+{ name: 'foo', title: 'Title of Colomn Foo', visible: true }
+```
+La propiedad name hace referencia a la propiedad a imprimir de un objeto del array data-model.
+La propiedad title indica cual sera el titulo de la columna en la tabla.
+La propiedad visible indica si la columna es visible
+
+####Objeto Modelo
+
+El objeto columna solo tiene que tener las propiedades a mostrar. La unica restriccion es que todas las propiedades motrar datos.
+
+##Propiedades
+
+Hay distintas propiedades que nos permiten hacer distintas cosas en la tabla y tambien para manejar eventos. A continuacion mencionaremos las propiedades de la tabla, como distintos eventos.
+
+###Propiedades de tabla
+
+######ClassName (class-name)
+La propiedad class-name de la tabla nos permite cambiar la propiedad class de la tabla para distintos usos. Puede ser desde aplicarle un plugin de jquery o cambiar el stilo. En el ejemplo de uso de la tabla utilizamos la propiedad para aplicarle estilo de tabla de bootstrap.
+
+```
+<vue-dynamic-table
+	class-name="table table-bordered table-hover"
+    v-bind:columns="myColumns"
+    v-bind:data-model="myDataModel">
+</vue-dynamic-table>
+```
+
+######PaginationOptions (pagination-options)
+La propiedad pagination-options de la tabla nos permite mostrar un paginador el cual sera personalizado de acuerdo a los valores de las propiedades del objeto asignado a esta.
+
+```
+<vue-dynamic-table
+	class-name="table table-bordered table-hover"
+    v-bind:columns="myColumns"
+    v-bind:data-model="myDataModel"
+    v-bind:pagination-options="myPaginationOptions">
+</vue-dynamic-table>
+```
+
+```
+var TestComponent = new Vue({
+	el: '#test-component',
+	data: {
+    	myColumns: [
+        	{ name:'col1', title:'Column 1', visible: true },
+        	{ name:'col2', title:'Column 2', visible: true },
+        	{ name:'col3', title:'Column 3', visible: true },
+        	{ name:'col4', title:'Column 4', visible: true }
+        ],
+        myDataModel: [
+        	{ col1: 'Value 1, 1', col2: 'Value 1, 2', col3: 'Value 1, 3', col4: 'Value 1, 4' },
+        	{ col1: 'Value 2, 1', col2: 'Value 2, 2', col3: 'Value 2, 3', col4: 'Value 2, 4' },
+        	{ col1: 'Value 3, 1', col2: 'Value 3, 2', col3: 'Value 3, 3', col4: 'Value 3, 4' },
+        	{ col1: 'Value 4, 1', col2: 'Value 4, 2', col3: 'Value 4, 3', col4: 'Value 4, 4' }
+        ],
+        myPaginationOptions: {
+        	position: 'top',
+            sizeOptionClass: 'form-control',
+            sizeOptions: [10, 20, 30, 40, 50],
+            size: 10,
+            paginatorClass: 'list-inline',
+            pageOptionClass: 'btn btn-primary',
+            page: 1
+        }
+    }
+});
+```
+|Propiedad|Descripcion|Valores|
+|:---|:---|:---|
+|**position**|Posiciona el paginador|String(top, bottom)|
+|**sizeOptionClass**|Clase que tendra el elemento select donde estan las opciones de tamaño de pagina|String|
+|**sizeOptions**|Array en donde se definen las opciones de tamaño de pagina|Array(Number*)|
+|**size**|Tamaño actual de la pagina. Este dato siempre sera el tamaño actual de la pagina, si se cambia el tamaño esta propiedad cambia|Number|
+|**paginatorClass**|Clase que se le aplicara a la lista de opciones del paginador|String|
+|**pageOptionClass**|Clase que se le aplicacara a un elemento de la lista de opciones del paginador|String|
+|**page**|Pagina actual. Este dato siempre sera la pagina actual.|Number|
+
+###Tipos de Columnas
+
+######Normal
+
+Una columna normal necesita solo las siguientes propiedades para poder mostrar informacion. Este tipo de columnas solo muestran el texto de forma normal.
+
+|Propiedad|Descripcion|Valores|
+|:---|:---|:---|
+|**name**|Es el nombre la propiedad del objeto fila que se mostrara|String|
+|**title**|Es el titulo a mostrar de la columna|String|
+|**visible**|Indica si la columna sra visible|Boolean, Default(false)|
+|**sorteable**|Indica si la columna puede ordenarse al dar click al titulo de la columna|Boolean|
+
+
+######Personalizada
+
+Esta columna se puede utilizar para imprimir informacion de forma personalizada. Por ejemplo si tenemos informacion sobre una url de una imagen, con esta columna podemos imprimir la imagen.
+
+|Propiedad|Descripcion|Valores|
+|:---|:---|:---|
+|**title**|Es el titulo a mostrar de la columna|String|
+|**visible**|Indica si la columna sra visible|Boolean, Default(false)|
+|**customRender**|Funcion que debe retonar el contido a mostrar en la celda|Function(row) -> String|
+
---
+++ vue-dynamic-table.js
@@ -0,0 +1,205 @@
+Vue.component('vue-dynamic-table', {
+	props: {
+		columns: Array,
+		className: String,
+		dataModel: Array,
+		sortIndicatorAsc: String,
+		sortIndicatorDesc: String,
+		pagination: Boolean,
+		paginationOptions: Object,
+		filterEnabled: Boolean,
+		actionLinkClick: Function,
+		actionButtonClick: Function
+	},
+	data: function() {
+		return {
+			sortColumn: null,
+			sortOrder: 'ASC'
+		}
+	},
+	methods: {
+		drag: function(column, event) {
+			var elemWidth  = event.target.offsetWidth;
+			if (event.offsetX >= elemWidth - 3 && event.offsetX <= elemWidth + 3) {
+				event.target.id = 'ASDASDADFASDAsd';
+				event.dataTransfer.setData('type', 'resize');
+				event.dataTransfer.setData('target', event.target.id);
+			} else {
+				event.dataTransfer.setData('type', 'move');
+				event.dataTransfer.setData('column', this.columns.indexOf(column));	
+			}
+		},
+		allowDrop: function(column, event) {
+			event.preventDefault();
+			var type = event.dataTransfer.getData('type');
+			
+			if (type == 'resize') {
+				var target = event.dataTransfer.getData('target');
+				//document.getElementById(target).style.width += '1px';
+			} else if (type == 'move') {
+				var indexColumn = this.columns.indexOf(column);
+				console.log('show indicator column', indexColumn);
+			}
+		},
+		drop: function(column, event) {
+			event.preventDefault();
+			var type = event.dataTransfer.getData('type');
+			
+			if (type == 'resize') {
+				console.log(event);
+				console.log(event.target);
+
+				var target = event.dataTransfer.getData('target');
+				var element = document.getElementById(target);
+				element.offsetWidth += 500;
+				element.id  = '';
+				console.log(element.offsetWidth);
+			} else if (type == 'move') {
+				var indexOrigin = event.dataTransfer.getData('column');
+				var indexTarget = this.columns.indexOf(column);
+
+				var aux = this.columns[indexOrigin];
+				
+				this.columns.splice(indexOrigin, 1);
+				this.columns.splice(indexTarget, 0, aux);
+			}
+		},
+        linkEvent: function(index, row, column) {
+			this.$emit('action-link-click', index, row, column);
+        },
+        buttonEvent: function(index, row, column) {
+			this.$emit('action-button-click', index, row, column);
+        },
+		sort: function(column) {
+			if (!column.sortable)
+				return;
+			if (this.sortColumn == column.name) {
+				if (this.sortOrder == 'ASC')
+					this.sortOrder = 'DESC'
+				else
+					this.sortOrder = 'ASC'
+			} else {
+				this.sortColumn = column.name;
+				this.sortOrder = 'ASC';
+			}
+
+			if (this.sortOrder == 'ASC') 
+				this.dataModel.sort(function(a, b) {
+					if (a[column.name] < b[column.name])
+						return -1;
+					if (a[column.name] > b[column.name])
+						return 1;
+					return 0;
+				});
+			else
+				this.dataModel.sort(function(a, b) {
+					if (a[column.name] > b[column.name])
+						return -1;
+					if (a[column.name] < b[column.name])
+						return 1;
+					return 0;
+				});
+		},
+		showSortIndicator: function(column) {
+			if (column.name == this.sortColumn)
+				if (this.sortOrder == 'ASC')
+					return this.sortIndicatorAsc;
+				else
+					return this.sortIndicatorDesc;
+			return '';
+		},
+		goToPage: function(page) {
+			if (page >= 1 && page <= this.paginationOptions.lastPage && page != this.paginationOptions.currentPage)
+				this.$emit('go-to-page', page);
+		},
+		changePageSize: function() {
+			this.paginationOptions.currentPage = 1;
+			this.$emit('go-to-page', this.paginationOptions.currentPage);
+			console.log(this.paginationOptions.size);
+		}
+	},
+	computed: {
+		visibleColumns: function() {
+			var visibleColumns = [];
+			for (var i = 0; i < this.columns.length; i ++) {
+				if (this.columns[i].visible)
+					visibleColumns.push(this.columns[i]);
+			}
+			return visibleColumns;
+		}
+	},
+	template: '\
+	<table v-bind:class="className">\
+		<thead>\
+			<template v-if="paginationOptions != null && paginationOptions.position == \'top\'">\
+				<tr>\
+					<td v-bind:colspan="visibleColumns.length">\
+						<select name="" id="" v-bind:class="paginationOptions.sizeOptionClass" v-model="paginationOptions.size" v-on:change="changePageSize(this)">\
+							<option v-for="option in paginationOptions.sizeOptions" v-bind:value="option">{{ option }}</option>\
+						</select>\
+						<ul v-bind:class="paginationOptions.paginatorClass">\
+							<li><a v-bind:class="paginationOptions.pageOptionClass" v-on:click="goToPage(1)">&laquo;</a></li>\
+							<li><a v-bind:class="paginationOptions.pageOptionClass" v-on:click="goToPage(paginationOptions.currentPage - 1)">&lsaquo;</a></li>\
+							<li><a v-bind:class="paginationOptions.pageOptionClass">{{ paginationOptions.currentPage }} de {{ paginationOptions.lastPage }}</a></li>\
+							<li><a v-bind:class="paginationOptions.pageOptionClass" v-on:click="goToPage(paginationOptions.currentPage + 1)">&rsaquo;</a></li>\
+							<li><a v-bind:class="paginationOptions.pageOptionClass" v-on:click="goToPage(paginationOptions.lastPage)">&raquo;</a></li>\
+						</ul>\
+					</td>\
+				</tr>\
+			</template>\
+			<tr>\
+				<th v-for="column in visibleColumns" v-bind:draggable="true" v-on:dragstart="drag(column, $event)" v-on:drop="drop(column, $event)" v-on:dragover="allowDrop(column, $event)"  v-on:click="sort(column)">{{ column.title }} <span v-html="showSortIndicator(column)"></span></th>\
+			</tr>\
+			<tr v-if="filterEnabled">\
+				<td v-for="column in visibleColumns"><input type="text" class="form-control" v-if="column.filterable"></td>\
+			</tr>\
+		</thead>\
+		<tbody>\
+			<tr v-for="(row, index) in dataModel">\
+				<td v-for="column in visibleColumns" v-bind:class="column.class" >\
+					<template v-if="typeof column.customRender == \'function\'">\
+						<div v-html="column.customRender(row)"></div>\
+					</template>\
+					<template v-else-if="column.actionLink != null">\
+						<a v-bind:class="column.actionLink.linkClass" v-on:click="linkEvent(index, row, column)"><span v-bind:class="column.actionLink.iconClass"></span> {{ column.actionLink.text }}</a>\
+					</template>\
+					<template v-else-if="column.actionButton != null">\
+						<button type="button" v-bind:class="column.actionButton.buttonClass" v-on:click="buttonEvent(index, row, column)"><span v-bind:class="column.actionButton.iconClass"></span> {{ column.actionButton.text }}</button>\
+					</template>\
+					<template v-else-if="column.input != null">\
+						<input type="text" v-model="row[column.name]" v-bind:class="column.input.inputClass" v-bind:placeholder="column.input.placeHolder" />\
+					</template>\
+					<template v-else-if="column.check != null">\
+						<input type="checkbox" v-model="row[column.name]" v-bind:class="column.check.checkClass" />\
+					</template>\
+					<template v-else-if="column.select != null">\
+						<select v-model="row[column.name]" v-bind:class="column.select.selectClass">\
+							<option v-for="option in column.select.selectOptions" v-bind:value="option.value">{{ option.text }}<option/>\
+						</select>\
+					</template>\
+					<template v-else>\
+						{{ row[column.name] }}\
+					</template>\
+				</td>\
+			</tr>\
+		</tbody>\
+		<tfoot>\
+			<template v-if="paginationOptions != null && paginationOptions.position == \'bottom\'">\
+				<tr>\
+					<td v-bind:colspan="visibleColumns.length">\
+						<select name="" id="" v-bind:class="paginationOptions.classSizeOption" v-model="paginationOptions.size" v-on:change="changePageSize(this)">\
+							<option v-for="option in paginationOptions.sizeOptions" v-bind:value="option">{{ option }}</option>\
+						</select>\
+						<ul v-bind:class="paginationOptions.classPaginator">\
+							<li><a v-bind:class="paginationOptions.classPageOption" v-on:click="goToPage(1)">&laquo;</a></li>\
+							<li><a v-bind:class="paginationOptions.classPageOption" v-on:click="goToPage(paginationOptions.currentPage - 1)">&lsaquo;</a></li>\
+							<li><a v-bind:class="paginationOptions.classPageOption">{{ paginationOptions.currentPage }} de {{ paginationOptions.lastPage }}</a></li>\
+							<li><a v-bind:class="paginationOptions.classPageOption" v-on:click="goToPage(paginationOptions.currentPage + 1)">&rsaquo;</a></li>\
+							<li><a v-bind:class="paginationOptions.classPageOption" v-on:click="goToPage(paginationOptions.lastPage)">&raquo;</a></li>\
+						</ul>\
+					</td>\
+				</tr>\
+			</template>\
+		</tfoot>\
+	</table>'
+});
